a26247105c666b567f9c798fa8344821e71aa9d5
MiltonMullins <miltonmullins92@gmail.com>
2025-01-08 10:08:01 -0300

    add Brute Force Solution, Hash Set Solution, Hash Set Length Solution to Contains Duplicate.

--- src/main/java/org/example/arrays/ContainsDuplicate.java
+++ src/main/java/org/example/arrays/ContainsDuplicate.java
@@ -18,9 +18,14 @@ Output: false
  */
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicate {
 
+    //Sorting Solution
+    //Time Complexity: O(nlogn)
+    //Space Complexity: O(1)
     public static boolean hasDuplicate(int[] nums){
         Arrays.sort(nums);
         for(int i=0;i< nums.length-1;i++){
@@ -29,5 +34,40 @@ public class ContainsDuplicate {
             }
         }
         return false;
+    }
+
+    //Hash Set Solution
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    public static boolean hasDuplicate2(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i< nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+
+    //Brute Force Solution
+    //Time Complexity: O(n^2)
+    //Space Complexity: O(1)
+    public static boolean hasDuplicate3(int[] nums){
+        for(int i=0;i< nums.length;i++){
+            for(int j=i+1;j< nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //Hash Set Length Solution
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    public static boolean hasDuplicate4(int[] nums){
+       return Arrays.stream(nums).distinct().count() != nums.length;
     }
 }
