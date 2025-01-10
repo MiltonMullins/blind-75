c8da13b3cfdb1acd320713bc1fad3a2f0c26a767
MiltonMullins <miltonmullins92@gmail.com>
2025-01-10 15:22:42 -0300

    add solution of Group of Anagrams problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
-import org.example.slidingWindow.SlidingWindow;
 import org.example.arrays.ContainsDuplicate;
 import org.example.arrays.TwoSum;
 import org.example.arrays.ValidAnagram;
+import org.example.arrays.groupAnagrams;
+import org.example.slidingWindow.SlidingWindow;
 import org.example.stack.ValidParentheses;
 import org.example.twoPointers.ValidPalindrome;
 
@@ -13,14 +14,14 @@ public class Main {
     public static void main(String[] args) {
 
         //Contains Duplicate
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {1, 2, 3, 4, 5};
         System.out.println("Contains Duplicate: " + ContainsDuplicate.hasDuplicate(nums));
 
 
         //Valid Anagram
         String s = "racecarcarracecarracecarracecarrace";
         String m = "carraceracecarracecarracecarracecar";
-        System.out.println("Valid Anagram: " + ValidAnagram.isAnagram(s,m));
+        System.out.println("Valid Anagram: " + ValidAnagram.isAnagram(s, m));
         //String test = "zxyfba";
         //char[] testChar = test.toCharArray();
         //int[] testInt = {4,3,2,5,1};
@@ -33,19 +34,23 @@ public class Main {
 
 
         //Two Sum
-        int[] nums2 = {5,5};
-        System.out.println("Two Sum: " + Arrays.toString(TwoSum.twoSum(nums2,10)));
+        int[] nums2 = {5, 5};
+        System.out.println("Two Sum: " + Arrays.toString(TwoSum.twoSum(nums2, 10)));
 
         //Valid Palindrome
         System.out.println("Valid Palindrome: " + ValidPalindrome.isPalindrome("tac cat"));
 
         //Slide Window
-        int[] prices = {10,7,5,2};
+        int[] prices = {10, 7, 5, 2};
         System.out.println("Max profit: " + SlidingWindow.maxProfit2(prices));
 
         //Valid Parentheses
         String str = "((])";
         System.out.println("Is Valid Parentheses: " + ValidParentheses.isValid(str));
+
+        //Group Anagrams
+        String[] strs = {"hat", "act", "pots", "tops", "cat", "stop"};
+        System.out.println("Has Group of Anagrams: " + groupAnagrams.hasGroupAnagrams(strs));
 
     }
 }
---
+++ src/main/java/org/example/arrays/groupAnagrams.java
@@ -0,0 +1,69 @@
+package org.example.arrays;
+
+/*
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+Example 2:
+
+Input: strs = ["x"]
+
+Output: [["x"]]
+Example 3:
+
+Input: strs = [""]
+
+Output: [[""]]
+Constraints:
+
+1 <= strs.length <= 1000.
+0 <= strs[i].length <= 100
+strs[i] is made up of lowercase English letters.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class groupAnagrams {
+
+    //Brute Force Solution made by Me
+    //Time Complexity: O(n ^2)
+    //Space Complexity: O(n)
+    public static List<List<String>> hasGroupAnagrams(String[] strs) {
+
+        List<List<String>> result = new ArrayList<>();
+        List<String> tempList = new ArrayList<>();
+
+        for (String str : strs) {
+            tempList.add(str);
+            if (result.stream().noneMatch(results -> results.contains(str))) {
+
+                char[] tempChar1 = tempList.get(0).toCharArray();
+                Arrays.sort(tempChar1);
+
+                for (String s : strs) {
+                    if (!s.equals(str)) {
+                        char[] tempChar2 = s.toCharArray();
+                        Arrays.sort(tempChar2);
+
+                        if (Arrays.equals(tempChar1, tempChar2)) {
+                            tempList.add(s);
+                        }
+                    }
+                }
+
+                result.add(tempList);
+            }
+            tempList = new ArrayList<>();
+
+        }
+        return result;
+    }
+}
